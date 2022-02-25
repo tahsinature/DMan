6cd910a02e20a69f1db68f2266b8a09cccd83831
Tahsin <tahsingenuies@gmail.com>
2022-02-25 21:56:15 +0700

    initialize repo

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/tahsinature/DMan/pkg/cli"
+)
+
+func main() {
+	cli.Setup()
+}
---
+++ pkg/cli/cli.go
@@ -0,0 +1,7 @@
+package cli
+
+import "fmt"
+
+func Setup() {
+	fmt.Println("setting up cli")
+}
